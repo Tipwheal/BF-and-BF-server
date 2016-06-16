be8c3589d3e0a9a4db9871709d715ec79d7895fa
Tipwheal <674714966@qq.com>
2016-06-16 16:10:42 +0800

    16.6.16.yzh.1

--- BF/src/runner/ClientRunner.java
+++ BF/src/runner/ClientRunner.java
@@ -10,42 +10,42 @@ import service.IOService;
 import ui.MainFrame;
 
 public class ClientRunner {
-	private RemoteHelper remoteHelper;
+    private RemoteHelper remoteHelper;
 
-	public ClientRunner() {
-		linkToServer();
-		initGUI();
-	}
+    public ClientRunner() {
+        linkToServer();
+        initGUI();
+    }
 
-	private void linkToServer() {
-		try {
-			remoteHelper = RemoteHelper.getInstance();
-			remoteHelper.setRemote(Naming.lookup("rmi://localhost:8888/DataRemoteObject"));
-			System.out.println("linked");
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		} catch (NotBoundException e) {
-			e.printStackTrace();
-		}
-	}
+    private void linkToServer() {
+        try {
+            remoteHelper = RemoteHelper.getInstance();
+            remoteHelper.setRemote(Naming.lookup("rmi://localhost:8888/DataRemoteObject"));
+            System.out.println("linked");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void initGUI() {
-		MainFrame mainFrame = new MainFrame();
-	}
+    private void initGUI() {
+        MainFrame mainFrame = new MainFrame();
+    }
 
-	public void test() {
-		try {
-			System.out.println(remoteHelper.getUserService().login("admin", "123456a"));
-			System.out.println(remoteHelper.getIOService().writeFile("2", "admin", "testFile"));
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-	}
+    public void test() {
+        try {
+            System.out.println(remoteHelper.getUserService().login("admin", "123456a"));
+            System.out.println(remoteHelper.getIOService().writeFile("2", "admin", "testFile"));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		ClientRunner cr = new ClientRunner();
-		// cr.test();
-	}
+    public static void main(String[] args) {
+        ClientRunner cr = new ClientRunner();
+        // cr.test();
+    }
 }
--- BF/src/ui/MainFrame.java
+++ BF/src/ui/MainFrame.java
@@ -9,78 +9,75 @@ import javax.swing.*;
 import rmi.RemoteHelper;
 
 public class MainFrame extends JFrame {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 377542095280692385L;
-	private JTextArea textArea;
-	private JLabel resultLabel;
+    private static final long serialVersionUID = 377542095280692385L;
+    private RemoteHelper helper = RemoteHelper.getInstance();
+    private JMenuItem runItem;
+    private JTextArea codeArea;
+    private JTextArea paramArea;
+    private JTextArea outputArea;
 
-	public MainFrame() {
-		// 创建窗体
-		JFrame frame = new JFrame("BF Client");
-		frame.setLayout(new BorderLayout());
+    public MainFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JMenuBar menuBar = new JMenuBar();
-		MyMenu fileMenu = new MyMenu("File");
-		menuBar.add(fileMenu);
-		fileMenu.addItems("New","Open","Save","Exit");
-		for(JMenuItem item:fileMenu.getItems()) {
-			item.addActionListener(new MenuItemActionListener());
-		}
-		frame.setJMenuBar(menuBar);
-		
-		MyMenu runMenu = new MyMenu("Run");
-		runMenu.addItems("Execute");
-		for(JMenuItem item:runMenu.getItems()) {
-			item.addActionListener(new MenuItemActionListener());
-		}
-		menuBar.add(runMenu);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenu runMenu = new JMenu("Run");
+        runItem = new JMenuItem("Run");
+        runItem.addActionListener(new MyActionListener());
+        runMenu.add(runItem);
+        JMenu whatMenu = new JMenu("What");
+        JMenu versionMenu = new JMenu("Version");
+        menuBar.add(fileMenu);
+        menuBar.add(runMenu);
+        menuBar.add(whatMenu);
+        menuBar.add(versionMenu);
+        this.setJMenuBar(menuBar);
 
-		textArea = new JTextArea();
-		textArea.setMargin(new Insets(10, 10, 10, 10));
-		textArea.setBackground(Color.LIGHT_GRAY);
-		frame.add(textArea, BorderLayout.CENTER);
+        codeArea = new JTextArea();
+        JScrollPane codeScrollPane = new JScrollPane(codeArea);
+        codeScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        codeScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        this.getContentPane().add(codeScrollPane, BorderLayout.CENTER);
 
-		// 显示结果
-		resultLabel = new JLabel();
-		resultLabel.setText("result");
-		frame.add(resultLabel, BorderLayout.SOUTH);
+        Box box = new Box(BoxLayout.X_AXIS);
+        paramArea = new JTextArea();
+        paramArea.setRows(6);
+        JScrollPane paramScrollPane = new JScrollPane(paramArea);
+        paramScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        paramScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        outputArea = new JTextArea();
+        outputArea.setRows(6);
+        outputArea.setEditable(false);
+        JScrollPane outputScrollPane = new JScrollPane(outputArea);
+        outputScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        outputScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        box.add(paramScrollPane);
+        box.add(outputScrollPane);
+        this.getContentPane().add(box, BorderLayout.SOUTH);
 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 400);
-		frame.setLocation(400, 200);
-		frame.setVisible(true);
-	}
-	
-	class MenuItemActionListener implements ActionListener {
-		/**
-		 * 子菜单响应事件
-		 */
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			String cmd = e.getActionCommand();
-			if (cmd.equals("Open")) {
-				textArea.setText("Open");
-			} else if (cmd.equals("Save")) {
-				textArea.setText("Save");
-			} else if (cmd.equals("Run")) {
-				resultLabel.setText("Hello, result");
-			}
-		}
-	}
+        int x = 600;
+        int y = 400;
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setBounds(((int) dimension.getWidth() - x) / 2, ((int) dimension.getHeight() - y) / 2, x, y);
+        this.setVisible(true);
+    }
 
-	class SaveActionListener implements ActionListener {
+    class MyActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == runItem) {
+                String code = codeArea.getText();
+                String param = paramArea.getText();
+                try {
+                    outputArea.setText(helper.getExecuteService().execute(code, param));
+                } catch (RemoteException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			String code = textArea.getText();
-			try {
-				RemoteHelper.getInstance().getIOService().writeFile(code, "admin", "code");
-			} catch (RemoteException e1) {
-				e1.printStackTrace();
-			}
-		}
+    public static void main(String[] args) {
+        new MainFrame();
+    }
 
-	}
 }
